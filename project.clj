12a2f1020298959c51fbf75c1640b3a41af2dcb1
David Santiago <david.santiago@gmail.com>
2012-07-15 13:23:28 -0700

    Add Codox to project.

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject scout "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Functional string searching and matching for parsing."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :plugins [[codox "0.6.2-SNAPSHOT"]]
+
+  :codox {:src-dir-uri "http://github.com/davidsantiago/scout/blob/master/"
+          :src-linenum-anchor-prefix "L"})
